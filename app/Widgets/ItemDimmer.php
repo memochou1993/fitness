7c76eefe9e70b6e3f1f5c8be220be39a0cb42e2f
vagrant <vagrant@homestead>
2018-11-24 14:58:49 +0000

    新增 ItemDimmer

---
+++ app/Widgets/ItemDimmer.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Widgets;
+
+use App\Item;
+use Illuminate\Support\Str;
+use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Widgets\BaseDimmer;
+use Illuminate\Support\Facades\Auth;
+
+class ItemDimmer extends BaseDimmer
+{
+    /**
+     * The configuration array.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Treat this method as a controller action.
+     * Return view() or other content to display.
+     */
+    public function run()
+    {
+        $count = Item::count();
+        $string = 'Items';
+
+        return view('voyager::dimmer', array_merge($this->config, [
+            'icon'   => 'voyager-group',
+            'title'  => $count . ' ' . $string,
+            'text'   => 'You have' . $count . 'items in your database. Click on button below to view all items.',
+            'button' => [
+                'text' => 'View all items',
+                'link' => route('voyager.items.index'),
+            ],
+            'image' => voyager_asset('images/widget-backgrounds/item.jpg'),
+        ]));
+    }
+
+    /**
+     * Determine if the widget should be displayed.
+     *
+     * @return bool
+     */
+    public function shouldBeDisplayed()
+    {
+        return Auth::user()->can('browse', app(Item::class));
+    }
+}
